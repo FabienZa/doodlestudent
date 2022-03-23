c2144fd2750e9901bb105e79318035159a2914e3
FabienZa <fabien@zaplana.fr>
2022-03-23 02:45:25 +0100

    add correct domain + sleep

--- up.sh
+++ up.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 cd traefik
  docker compose up -d
+ ## sleep in bash for loop ##
+for i in {1..10}
+do
+  echo "Sleeping some seconds because server is too slow..."
+  sleep 1s
+done
 cd ../etherpad
  docker compose up -d
+ for i in {1..10}
+do
+  echo "Sleeping some seconds because server is too slow..."
+  sleep 1s
+done
 cd ../db
 docker compose up -d
+for i in {1..10}
+do
+  echo "Sleeping some seconds because server is too slow..."
+  sleep 1s
+done
 cd ../mail
 docker compose up -d
+for i in {1..10}
+do
+  echo "Sleeping some seconds because server is too slow..."
+  sleep 1s
+done
 cd ../api
 docker compose up -d
+for i in {1..10}
+do
+  echo "Sleeping some seconds because server is too slow..."
+  sleep 1s
+done
 cd ../front
 docker compose up -d
